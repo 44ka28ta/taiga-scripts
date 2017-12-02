cdec0bcb23ce621efc757b556fd362c0f4fe44cd
44ka28ta <44ka28ta@gmail.com>
2017-12-02 21:33:34 +0900

    Fix the path of virtualenvwrapper
    Fix the path of virtualenvwrapper.

--- scripts/setup-python.sh
+++ scripts/setup-python.sh
@@ -2,7 +2,7 @@
 
 #apt_install_if_needed python3 python3-pip python-dev python3-dev python-pip virtualenvwrapper libxml2-dev libxslt1-dev
 apt_install python3 python3-pip python-devel python3-devel python-pip python-virtualenvwrapper libxml2-devel libxslt-devel
-source /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
+source /usr/bin/virtualenvwrapper_lazy.sh
 
 function mkvirtualenv_if_needed {
     for envname in $@; do
