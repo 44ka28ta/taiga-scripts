2b2f6485cb1f7a494748b3adc5c733a16becbe02
44ka28ta <44ka28ta@gmail.com>
2017-12-02 21:21:10 +0900

    Fix nonexist package
    Fix nonexist package.

--- scripts/setup-postgresql.sh
+++ scripts/setup-postgresql.sh
@@ -13,8 +13,10 @@ function dropdb_if_needed {
 }
 
 if [ ! -e ~/.setup/postgresql ]; then
+    #apt_install postgresql95 postgresql95-contrib \
+    #    postgresql95-doc postgresql95-server postgresql95-devel
     apt_install postgresql95 postgresql95-contrib \
-        postgresql95-doc postgresql95-server postgresql95-devel
+        postgresql95-server postgresql95-devel
 
     sudo -u postgres createuser --superuser $USER &> /dev/null
     sudo -u postgres createdb $USER &> /dev/null
