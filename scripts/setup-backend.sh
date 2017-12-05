0cec84164d758140561223da295c4d13a723c899
44ka28ta <44ka28ta@gmail.com>
2017-12-05 22:43:08 +0900

    Fix origin url
    Fix origin url.

--- scripts/setup-backend.sh
+++ scripts/setup-backend.sh
@@ -12,7 +12,7 @@ STATIC_URL = "$hostname/static/"
 
 # This should change if you want generate urls in emails
 # for external dns.
-SITES["front"]["domain"] = "$hostname"
+SITES["front"]["domain"] = "$hostname:8000"
 
 DEBUG = True
 PUBLIC_REGISTER_ENABLED = True
